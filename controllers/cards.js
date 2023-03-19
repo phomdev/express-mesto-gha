367b5befeb21f702541b5fcdca615f1d5e2a089b
Artem Rudenko <phomdev@yandex.ru>
2023-03-20 01:45:35 +0300

    pretest 7.71 card remove3

--- controllers/cards.js
+++ controllers/cards.js
@@ -35,12 +35,9 @@ const createCard = (req, res, next) => {
 const deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
     .then((selectedCard) => {
-      if (!selectedCard) {
-        throw new NotFound('Карточка по указанному _id не найдена');
-      }
-      if (!selectedCard.owner.equals(req.user._id)) {
-        throw new Forbidden('Вы не являетесь автором карточки, удаление невозможно');
-      }
+      const cardOwner = selectedCard.owner.equals(req.user._id);
+      if (!selectedCard) { next(new NotFound('Карточка по указанному _id не найдена')); }
+      if (!cardOwner) { next(new Forbidden('Вы не являетесь автором карточки, удаление невозможно')); }
       Card.findByIdAndDelete(req.params.cardId)
         .orFail(() => new NotFound('Карточка по указанному _id не найдена'))
         .then(() => { res.send({ message: 'Карточка успешно удалена с сервера' }); });
