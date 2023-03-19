d002a8b2b4e9b292d1ddc9ec981955d0577a35c7
Artem Rudenko <phomdev@yandex.ru>
2023-03-20 00:06:51 +0300

    pretest 7,1

--- controllers/users.js
+++ controllers/users.js
@@ -42,10 +42,10 @@ const createUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
-  const passwordHash = bcrypt.hash(password, 10);
-  User.create({
-    name, about, avatar, email, password: passwordHash,
-  })
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
     .then((userObject) => res.status(SUCCESS_CREATED).send({ data: userObject }))
     .catch((error) => {
       // https://mongoosejs.com/docs/api/error.html#error_Error-ValidationError
